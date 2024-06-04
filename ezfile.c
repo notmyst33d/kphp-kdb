2e113bab2afe20566025968c4da796224211e186
Myst33d <notmyst33d@gmail.com>
2024-06-04 18:36:03 +0600

    Refactor ez

--- ezfile.c
+++ ezfile.c
@@ -2,33 +2,12 @@
 
 #include "ez.h"
 
-void cc(char *in, char *out) {
-    Command *cmd = cmd_new();
-    char message[512];
+char *compile_flags[] = { "-std=gnu89", "-fPIC", "-fcommon", "-I.", "-Ivv", "-Ikfs", "-Inet", "-Iobjs", "-Icommon", "-Ibinlog", "-I/usr/include/openssl-1.1", "-DCOMMIT=\"none\"", 0 };
+char *shared_lib[] = { "-shared", 0 };
+char *binary[] = { "-Lout", "-lm", "-lz", "-lcrypto", "-lkdb-binlog", "-lkdb-common", "-lkdb-crypto", "-lkdb-kfs", "-lkdb-net", "-lkdb-vv", 0 };
 
-    sprintf(message, "cc %s", in);
-    cmd_message(cmd, message);
-
-    cmd_push(cmd, "cc");
-    cmd_push(cmd, "-std=gnu89");
-    cmd_push(cmd, "-fPIC");
-    cmd_push(cmd, "-fcommon");
-    cmd_push(cmd, "-I.");
-    cmd_push(cmd, "-Ivv");
-    cmd_push(cmd, "-Ikfs");
-    cmd_push(cmd, "-Inet");
-    cmd_push(cmd, "-Iobjs");
-    cmd_push(cmd, "-Icommon");
-    cmd_push(cmd, "-Ibinlog");
-    cmd_push(cmd, "-I/usr/include/openssl-1.1");
-    cmd_push(cmd, "-DCOMMIT=\"none\"");
-    cmd_push(cmd, "-c");
-    cmd_push(cmd, "-o");
-    cmd_push(cmd, out);
-    cmd_push(cmd, in);
-
-    queue_push(&global_queue, cmd);
-}
+#define cc(in, out) global_queue_cc(in, out, compile_flags)
+#define ld global_queue_ld
 
 void build_kdb_common(void) {
     cc("common/aho-kmp.c", "out/common/aho-kmp.o");
@@ -57,39 +36,33 @@ void build_kdb_common(void) {
     cc("common/xz_dec.c", "out/common/xz_dec.o");
 }
 
-int link_kdb_common(void) {
-    Command *cmd = cmd_new();
-    cmd_push(cmd, "cc");
-    cmd_push(cmd, "-shared");
-    cmd_push(cmd, "-o");
-    cmd_push(cmd, "out/libkdb-common.so");
-    cmd_push(cmd, "out/common/aho-kmp.o");
-    cmd_push(cmd, "out/common/base64.o");
-    cmd_push(cmd, "out/common/common-data.o");
-    cmd_push(cmd, "out/common/crc32.o");
-    cmd_push(cmd, "out/common/crc32c.o");
-    cmd_push(cmd, "out/common/diff-patch.o");
-    cmd_push(cmd, "out/common/estimate-split.o");
-    cmd_push(cmd, "out/common/fast-backtrace.o");
-    cmd_push(cmd, "out/common/kdb-data-common.o");
-    cmd_push(cmd, "out/common/listcomp.o");
-    cmd_push(cmd, "out/common/md5.o");
-    cmd_push(cmd, "out/common/pid.o");
-    cmd_push(cmd, "out/common/resolver.o");
-    cmd_push(cmd, "out/common/search-y-parse.o");
-    cmd_push(cmd, "out/common/server-functions.o");
-    cmd_push(cmd, "out/common/sha1.o");
-    cmd_push(cmd, "out/common/stemmer-new.o");
-    cmd_push(cmd, "out/common/string-processing.o");
-    cmd_push(cmd, "out/common/suffix-array.o");
-    cmd_push(cmd, "out/common/translit.o");
-    cmd_push(cmd, "out/common/unicode-utils.o");
-    cmd_push(cmd, "out/common/utf8_utils.o");
-    cmd_push(cmd, "out/common/word-split.o");
-    cmd_push(cmd, "out/common/xz_dec.o");
-    assert(cmd_execute(cmd));
-    cmd_free(cmd);
-    return 0;
+void link_kdb_common(void) {
+    ld("out/libkdb-common.so", (char*[]){
+        "out/common/aho-kmp.o",
+        "out/common/base64.o",
+        "out/common/common-data.o",
+        "out/common/crc32.o",
+        "out/common/crc32c.o",
+        "out/common/diff-patch.o",
+        "out/common/estimate-split.o",
+        "out/common/fast-backtrace.o",
+        "out/common/kdb-data-common.o",
+        "out/common/listcomp.o",
+        "out/common/md5.o",
+        "out/common/pid.o",
+        "out/common/resolver.o",
+        "out/common/search-y-parse.o",
+        "out/common/server-functions.o",
+        "out/common/sha1.o",
+        "out/common/stemmer-new.o",
+        "out/common/string-processing.o",
+        "out/common/suffix-array.o",
+        "out/common/translit.o",
+        "out/common/unicode-utils.o",
+        "out/common/utf8_utils.o",
+        "out/common/word-split.o",
+        "out/common/xz_dec.o", 0
+    }, shared_lib);
 }
 
 void build_kdb_vv(void) {
@@ -101,37 +74,23 @@ void build_kdb_vv(void) {
     cc("vv/vv-tl-parse.c", "out/vv/vv-tl-parse.o");
 }
 
-int link_kdb_vv(void) {
-    Command *cmd = cmd_new();
-    cmd_push(cmd, "cc");
-    cmd_push(cmd, "-shared");
-    cmd_push(cmd, "-o");
-    cmd_push(cmd, "out/libkdb-vv.so");
-    cmd_push(cmd, "out/vv/am-amortization.o");
-    cmd_push(cmd, "out/vv/am-hash.o");
-    cmd_push(cmd, "out/vv/am-server-functions.o");
-    cmd_push(cmd, "out/vv/am-stats.o");
-    cmd_push(cmd, "out/vv/vv-tl-aio.o");
-    cmd_push(cmd, "out/vv/vv-tl-parse.o");
-    assert(cmd_execute(cmd));
-    cmd_free(cmd);
-    return 0;
+void link_kdb_vv(void) {
+    ld("out/libkdb-vv.so", (char*[]){
+        "out/vv/am-amortization.o",
+        "out/vv/am-hash.o",
+        "out/vv/am-server-functions.o",
+        "out/vv/am-stats.o",
+        "out/vv/vv-tl-aio.o",
+        "out/vv/vv-tl-parse.o", 0
+    }, shared_lib);
 }
 
 void build_kdb_crypto(void) {
     cc("crypto/aesni256.c", "out/crypto/aesni256.o");
 }
 
-int link_kdb_crypto(void) {
-    Command *cmd = cmd_new();
-    cmd_push(cmd, "cc");
-    cmd_push(cmd, "-shared");
-    cmd_push(cmd, "-o");
-    cmd_push(cmd, "out/libkdb-crypto.so");
-    cmd_push(cmd, "out/crypto/aesni256.o");
-    assert(cmd_execute(cmd));
-    cmd_free(cmd);
-    return 0;
+void link_kdb_crypto(void) {
+    ld("out/libkdb-crypto.so", (char*[]){ "out/crypto/aesni256.o", 0 }, shared_lib);
 }
 
 void build_kdb_net(void) {
@@ -155,64 +114,43 @@ void build_kdb_net(void) {
     cc("net/net-udp-targets.c", "out/net/net-udp-targets.o");
 }
 
-int link_kdb_net(void) {
-    Command *cmd = cmd_new();
-    cmd_push(cmd, "cc");
-    cmd_push(cmd, "-shared");
-    cmd_push(cmd, "-o");
-    cmd_push(cmd, "out/libkdb-net.so");
-    cmd_push(cmd, "out/net/net-buffers.o");
-    cmd_push(cmd, "out/net/net-connections.o");
-    cmd_push(cmd, "out/net/net-crypto-aes.o");
-    cmd_push(cmd, "out/net/net-crypto-rsa.o");
-    cmd_push(cmd, "out/net/net-events.o");
-    cmd_push(cmd, "out/net/net-memcache-server.o");
-    cmd_push(cmd, "out/net/net-msg-buffers.o");
-    cmd_push(cmd, "out/net/net-msg.o");
-    cmd_push(cmd, "out/net/net-parse.o");
-    cmd_push(cmd, "out/net/net-rpc-common.o");
-    cmd_push(cmd, "out/net/net-rpc-server.o");
-    cmd_push(cmd, "out/net/net-rpc-targets.o");
-    cmd_push(cmd, "out/net/net-tcp-connections.o");
-    cmd_push(cmd, "out/net/net-tcp-rpc-common.o");
-    cmd_push(cmd, "out/net/net-tcp-rpc-server.o");
-    cmd_push(cmd, "out/net/net-udp.o");
-    cmd_push(cmd, "out/net/net-udp-targets.o");
-    assert(cmd_execute(cmd));
-    cmd_free(cmd);
-    return 0;
+void link_kdb_net(void) {
+    ld("out/libkdb-net.so", (char*[]){
+        "out/net/net-aio.o",
+        "out/net/net-buffers.o",
+        "out/net/net-connections.o",
+        "out/net/net-crypto-aes.o",
+        "out/net/net-crypto-rsa.o",
+        "out/net/net-events.o",
+        "out/net/net-memcache-server.o",
+        "out/net/net-msg-buffers.o",
+        "out/net/net-msg.o",
+        "out/net/net-parse.o",
+        "out/net/net-rpc-common.o",
+        "out/net/net-rpc-server.o",
+        "out/net/net-rpc-targets.o",
+        "out/net/net-tcp-connections.o",
+        "out/net/net-tcp-rpc-common.o",
+        "out/net/net-tcp-rpc-server.o",
+        "out/net/net-udp.o",
+        "out/net/net-udp-targets.o", 0
+    }, shared_lib);
 }
 
 void build_kdb_binlog(void) {
     cc("binlog/kdb-binlog-common.c", "out/binlog/kdb-binlog-common.o");
 }
 
-int link_kdb_binlog(void) {
-    Command *cmd = cmd_new();
-    cmd_push(cmd, "cc");
-    cmd_push(cmd, "-shared");
-    cmd_push(cmd, "-o");
-    cmd_push(cmd, "out/libkdb-binlog.so");
-    cmd_push(cmd, "out/binlog/kdb-binlog-common.o");
-    assert(cmd_execute(cmd));
-    cmd_free(cmd);
-    return 0;
+void link_kdb_binlog(void) {
+    ld("out/libkdb-binlog.so", (char*[]){ "out/binlog/kdb-binlog-common.o", 0 }, shared_lib);
 }
 
 void build_kdb_kfs(void) {
     cc("kfs/kfs.c", "out/kfs/kfs.o");
 }
 
-int link_kdb_kfs(void) {
-    Command *cmd = cmd_new();
-    cmd_push(cmd, "cc");
-    cmd_push(cmd, "-shared");
-    cmd_push(cmd, "-o");
-    cmd_push(cmd, "out/libkdb-kfs.so");
-    cmd_push(cmd, "out/kfs/kfs.o");
-    assert(cmd_execute(cmd));
-    cmd_free(cmd);
-    return 0;
+void link_kdb_kfs(void) {
+    ld("out/libkdb-kfs.so", (char*[]){ "out/kfs/kfs.o", 0 }, shared_lib);
 }
 
 void build_random_engine(void) {
@@ -220,26 +158,11 @@ void build_random_engine(void) {
     cc("random/random-engine.c", "out/random/random-engine.o");
 }
 
-int link_random_engine(void) {
-    Command *cmd = cmd_new();
-    cmd_push(cmd, "cc");
-    cmd_push(cmd, "-Lout");
-    cmd_push(cmd, "-lm");
-    cmd_push(cmd, "-lz");
-    cmd_push(cmd, "-lcrypto");
-    cmd_push(cmd, "-lkdb-binlog");
-    cmd_push(cmd, "-lkdb-common");
-    cmd_push(cmd, "-lkdb-crypto");
-    cmd_push(cmd, "-lkdb-kfs");
-    cmd_push(cmd, "-lkdb-net");
-    cmd_push(cmd, "-lkdb-vv");
-    cmd_push(cmd, "-o");
-    cmd_push(cmd, "out/random-engine");
-    cmd_push(cmd, "out/random/random-data.o");
-    cmd_push(cmd, "out/random/random-engine.o");
-    assert(cmd_execute(cmd));
-    cmd_free(cmd);
-    return 0;
+void link_random_engine(void) {
+    ld("out/random-engine", (char*[]){
+        "out/random/random-data.o",
+        "out/random/random-engine.o", 0
+    }, binary);
 }
 
 void build_dns_engine(void) {
@@ -247,26 +170,23 @@ void build_dns_engine(void) {
     cc("dns/dns-engine.c", "out/dns/dns-engine.o");
 }
 
-int link_dns_engine(void) {
-    Command *cmd = cmd_new();
-    cmd_push(cmd, "cc");
-    cmd_push(cmd, "-Lout");
-    cmd_push(cmd, "-lm");
-    cmd_push(cmd, "-lz");
-    cmd_push(cmd, "-lcrypto");
-    cmd_push(cmd, "-lkdb-binlog");
-    cmd_push(cmd, "-lkdb-common");
-    cmd_push(cmd, "-lkdb-crypto");
-    cmd_push(cmd, "-lkdb-kfs");
-    cmd_push(cmd, "-lkdb-net");
-    cmd_push(cmd, "-lkdb-vv");
-    cmd_push(cmd, "-o");
-    cmd_push(cmd, "out/dns-engine");
-    cmd_push(cmd, "out/dns/dns-data.o");
-    cmd_push(cmd, "out/dns/dns-engine.o");
-    assert(cmd_execute(cmd));
-    cmd_free(cmd);
-    return 0;
+void link_dns_engine(void) {
+    ld("out/dns-engine", (char*[]){
+        "out/dns/dns-data.o",
+        "out/dns/dns-engine.o", 0
+    }, binary);
+}
+
+void build_lists_engine(void) {
+    cc("lists/lists-data.c", "out/lists/lists-data.o");
+    cc("lists/lists-engine.c", "out/lists/lists-engine.o");
+}
+
+void link_lists_engine(void) {
+    ld("out/lists-engine", (char*[]){
+        "out/lists/lists-data.o",
+        "out/lists/lists-engine.o", 0
+    }, binary);
 }
 
 int main(void) {
@@ -279,10 +199,9 @@ int main(void) {
     ez_mkdir("out/kfs");
     ez_mkdir("out/random");
     ez_mkdir("out/dns");
+    ez_mkdir("out/lists");
 
-    for (int i = 0; i < MAX_THREADS; i++) {
-        pthread_create(&thread_pool[i], 0, queue_worker, 0);
-    }
+    global_queue_start();
 
     build_kdb_common();
     build_kdb_vv();
@@ -292,25 +211,23 @@ int main(void) {
     build_kdb_kfs();
     build_random_engine();
     build_dns_engine();
+    build_lists_engine();
+    assert(global_queue_wait());
 
-    while (global_queue.size > 0 && !fail) {
-        usleep(1000);
-    }
-    if (fail) {
-        ERROR("Compilation aborted");
-        queue_shutdown();
-        return -1;
-    }
-    queue_shutdown();
+    link_kdb_common();
+    link_kdb_vv();
+    link_kdb_crypto();
+    link_kdb_net();
+    link_kdb_binlog();
+    link_kdb_kfs();
+    assert(global_queue_wait());
 
-    assert(link_kdb_common());
-    assert(link_kdb_vv());
-    assert(link_kdb_crypto());
-    assert(link_kdb_net());
-    assert(link_kdb_binlog());
-    assert(link_kdb_kfs());
-    assert(link_random_engine());
-    assert(link_dns_engine());
+    link_random_engine();
+    link_dns_engine();
+    link_lists_engine();
+    assert(global_queue_wait());
+
+    global_queue_shutdown();
 
     INFO("Done");
     return 0;
